ce66ed0110f50cd45b28dac3a7731dbc43c60510
Belo <eduardo.belo@accenture.com>
2019-05-29 15:32:34 -0300

    Fazendo callback de maneira diferente

--- main2.js
+++ main2.js
@@ -1,150 +1,162 @@
-const Users = [
-    {
-        "gender": "male",
-        "name": {
-            "title": "mr",
-            "first": "rolf",
-            "last": "hegdal"
+(function(){
+    const Users = [
+        {
+            "gender": "male",
+            "name": {
+                "title": "mr",
+                "first": "rolf",
+                "last": "hegdal"
+            },
+            "email": "rolf.hegdal@example.com"
         },
-        "email": "rolf.hegdal@example.com"
-    },
-    {
-        "gender": "male",
-        "name": {
-            "title": "mr",
-            "first": "darth",
-            "last": "hebras"
+        {
+            "gender": "male",
+            "name": {
+                "title": "mr",
+                "first": "darth",
+                "last": "hebras"
+            },
+            "email": "darth.hebras@example.com",
         },
-        "email": "darth.hebras@example.com",
-    },
-    {
-        "gender": "male",
-        "name": {
-            "title": "mr",
-            "first": "tifal",
-            "last": "hood"
+        {
+            "gender": "male",
+            "name": {
+                "title": "mr",
+                "first": "tifal",
+                "last": "hood"
+            },
+            "email": "tifal.hood@example.com"
         },
-        "email": "tifal.hood@example.com"
-    },
-    {
-        "gender": "male",
-        "name": {
-            "title": "mr",
-            "first": "kyle",
-            "last": "laurence"
+        {
+            "gender": "male",
+            "name": {
+                "title": "mr",
+                "first": "kyle",
+                "last": "laurence"
+            },
+            "email": "kyle.laurence@example.com"
+        }
+    ];
+    
+    const Infos = [
+        {
+            "zipcode": "24011",
+            "email": "rolf.hegdal@example.com",
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/60.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/60.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/60.jpg"
+            }
         },
-        "email": "kyle.laurence@example.com"
-    }
-];
-
-const Infos = [
-    {
-        "zipcode": "24011",
-        "email": "rolf.hegdal@example.com",
-        "picture": {
-            "large": "https://randomuser.me/api/portraits/men/60.jpg",
-            "medium": "https://randomuser.me/api/portraits/med/men/60.jpg",
-            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/60.jpg"
+        {
+            "zipcode": "38930",
+            "email": "darth.hebras@example.com",
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/65.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/65.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/65.jpg"
+            }
+        },
+        {
+            "zipcode": "07645",
+            "email": "tifal.hood@example.com",
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/13.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/13.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/13.jpg"
+            }
+        },
+        {
+            "zipcode": "12210",
+            "email": "kyle.laurence@example.com",
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/men/10.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/men/10.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/10.jpg"
+            }
         }
-    },
-    {
-        "zipcode": "38930",
-        "email": "darth.hebras@example.com",
-        "picture": {
-            "large": "https://randomuser.me/api/portraits/men/65.jpg",
-            "medium": "https://randomuser.me/api/portraits/med/men/65.jpg",
-            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/65.jpg"
+    ]
+    
+    const Locations = [
+        {
+            "zipcode": "24011",
+            "location": {
+                "street": "1403  Goldcliff Circle",
+                "city": "Washington",
+                "state": "DC"
+            }
+        },
+        {
+            "zipcode": "38930",
+            "location": {
+                "street": "3011  Margaret Street",
+                "city": "CROWELL",
+                "state": "TX"
+            }
+        },
+        {
+            "zipcode": "07645",
+            "location": {
+                "street": "1752  Wetzel Lane",
+                "city": "MONTVALE",
+                "state": "NJ"
+            }
+        },
+        {
+            "zipcode": "12210",
+            "location": {
+                "street": "1056  West Virginia Avenue",
+                "city": "Albany",
+                "state": "NY"
+            }
         }
-    },
-    {
-        "zipcode": "07645",
-        "email": "tifal.hood@example.com",
-        "picture": {
-            "large": "https://randomuser.me/api/portraits/men/13.jpg",
-            "medium": "https://randomuser.me/api/portraits/med/men/13.jpg",
-            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/13.jpg"
-        }
-    },
-    {
-        "zipcode": "12210",
-        "email": "kyle.laurence@example.com",
-        "picture": {
-            "large": "https://randomuser.me/api/portraits/men/10.jpg",
-            "medium": "https://randomuser.me/api/portraits/med/men/10.jpg",
-            "thumbnail": "https://randomuser.me/api/portraits/thumb/men/10.jpg"
-        }
-    }
-]
-
-const Locations = [
-    {
-        "zipcode": "24011",
-        "location": {
-            "street": "1403  Goldcliff Circle",
-            "city": "Washington",
-            "state": "DC"
-        }
-    },
-    {
-        "zipcode": "38930",
-        "location": {
-            "street": "3011  Margaret Street",
-            "city": "CROWELL",
-            "state": "TX"
-        }
-    },
-    {
-        "zipcode": "07645",
-        "location": {
-            "street": "1752  Wetzel Lane",
-            "city": "MONTVALE",
-            "state": "NJ"
-        }
-    },
-    {
-        "zipcode": "12210",
-        "location": {
-            "street": "1056  West Virginia Avenue",
-            "city": "Albany",
-            "state": "NY"
-        }
-    }
-]
-
-
-
-const getUsers = async (callback) => {
-    for (user of Users){
-        return callback(user);
-    }
-}
-
-const getInfos = async (email, callback) => {
-    for (infos of Infos){
-        if (email.email == infos.email){
-            return callback(infos);
-        } 
-    }
-}
-
-const getLocations = async (zipcode, callback) => {
-    for (locate of Locations){
-        if (zipcode.zipcode == locate.zipcode){
-            return callback(locate);
+    ]
+    
+    let lista = [];
+    const getUsers = async (callback) => {
+        
+        // userData = Object.assign(Users,userData); 
+        for (user of Users){
+            callback(user);  
         }    
-    }    
-}
-
-
-
-    getUsers( (users) => {
-        userData = [];
-        userData = Object.assign(users,userData);
-        getInfos(userData, (infos) => {
-            userData = Object.assign(userData,infos);
-            getLocations(userData, (locate) => {
-                userData = Object.assign(userData,locate); 
-                return console.log(userData);
+    }
+    
+    const getInfos = async (userData, callback) => {
+        //console.log('userDataAntes: ', userData);
+        for (info of Infos){
+            if(userData.email == info.email) {
+               // userData.zipcode = info.zipcode;  
+                //userData.picture = info.picture; 
+                userData = Object.assign(info,userData);   
+            }  
+        };
+        
+        // console.log('userDataDepois: ', userData);
+        callback(userData)  
+       
+    }
+    
+    const getLocations = async (userData, callback) => {
+       for (loca of Locations){ 
+            if (userData.zipcode == loca.zipcode){
+                userData.location = loca.location;
+            } 
+        }
+        lista.push(userData)
+        
+        return callback(userData);
+        
+    }
+    
+    
+    
+        getUsers( function (users) {
+            getInfos(users, function resolveInfos(infos)  {
+                getLocations(infos, function  resolveLocation(userData) {
+                    return lista;
+                })
             })
-        })
-    });
+        });
+    
+        console.log(lista)
+})();
